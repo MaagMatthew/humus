2829f5a204093baec129f808306b9195b9ef2cc6
Kevin <32335809+k3vlar82@users.noreply.github.com>
2018-03-09 11:39:56 -0700

    about

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var canvas = document.getElementById('barGraph');
 var ctx = canvas.getContext('2d');
-var dim = 500;
-var border = 100;
-var xSpace= 105;
-var lineSpace = 100;
+var dim = 250;
+var border = 50;
+var xSpace= 52.5;
+var lineSpace = 50;
 var reviews= [11, 4, 5, 3, 2];
 // 5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,3,3,3,3,3,2,2,2,1,1
 
 function drawRect()
 {
     ctx.fillStyle = "#7e3896";
-    ctx.fillRect(border, 25, dim, dim);
+    ctx.fillRect(border, 12.5, dim, dim);
     ctx.fill();
 }
 
@@ -21,7 +21,7 @@ function drawBars()
     ctx.fillStyle = "#f8d267";
     for(var i = 0; i<reviews.length; i++)
     {
-    ctx.fillRect(border,(xSpace*i+40),(reviews[i] * 20),50);
+    ctx.fillRect(border,(xSpace*i+20),((reviews[i]) * 10),25);
     }
     ctx.fill();
 }
@@ -32,7 +32,7 @@ function drawLines()
     ctx.fillStyle = "#f8d267";
     for(var i = 0; i<reviews.length; i++)
     {
-        ctx.fillRect((border + (lineSpace*i)),25,1,dim);
+        ctx.fillRect((border + (lineSpace*i)),12.5,1,dim);
     }
     ctx.fill();
 }
@@ -40,12 +40,12 @@ drawLines();
 
 function labels()
 {
-    ctx.font = "25px Arial";
+    ctx.font = "20px Arial";
     ctx.fillStyle = "#7e3896";
 
     for(var i = 0; i<reviews.length; i++)
     {
-        ctx.fillText( (i+1) + " Stars",10,(dim-(105*(i))));
+        ctx.fillText( (i+1) + " Stars", 5,(dim-(52.5*(i))));
     }
 }
 labels();
